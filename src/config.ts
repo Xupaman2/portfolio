1129b1f7ca9e7774e0507a21b98c7cfa66211639
Xupaman2 <sue.pha26@gmail.com>
2025-05-22 02:18:45 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,4 @@
 export const SITE = {
-  ...
   name: "Sue Pha",
   tagline: "Aspiring Software Developer & Data Analyst",
   description: "Turning data into insights and automation into impact.",
-  ...
-},
-  dynamicOgImage: true,
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
-} as const;
